9557db10d84444adc9793c4374fb357feac01b9b
Etienne <emoureton@gmail.com>
2022-09-14 20:55:37 +0200

    improve SEO

--- components/Support.vue
+++ components/Support.vue
@@ -1,13 +1,13 @@
 <template>
-  <a href="https://www.buymeacoffee.com/etiennemoureton" target="_blank" class="fixed bottom-10 right-10 p-4 rounded-full flex items-center gap-4 bg-slate-50 shadow-2xl">
+  <div class="fixed bottom-10 right-10 p-4 rounded-full flex items-center gap-4 bg-slate-50 shadow-2xl">
     <div class="w-[2.5rem] h-fit p-2 rounded-full overflow-hidden bg-[#f8dd03]">
       <img class="w-full" src="~/assets/img/coffee.png" alt="buy me a coffee logo">
     </div>
     <div class="flex flex-col justify-center mr-5">
       <p class="text-xs text-slate-500">Liked using my tool ?</p>
-      <p class="text-base">Buy me a coffee</p>
+      <a href="https://www.buymeacoffee.com/etiennemoureton" target="_blank" class="text-base">Buy me a coffee</a>
     </div>
-  </a>
+  </div>
 </template>
 
 <script setup>
