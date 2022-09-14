57b40a12abb3785eedbb4734a8857631c13fca4c
Etienne <emoureton@gmail.com>
2022-09-14 20:58:26 +0200

    improve SEO

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="w-full mt-auto mb-0">
     <div class="w-full flex justify-between align-center">
       <p class="text-sm">Made with <span>&#10084</span> by <a href="https://etiennemoureton.fr" target="_blank">Etienne Moureton</a></p>
-      <p class="text-sm"><a class="flex gap-2 align-center" href="https://github.com/YsarocK/mansorygrid-maker" target="_blank"><GithubIcon class="w-[1rem]" /></a></p>
+      <p class="text-sm"><a class="flex gap-2 align-center" href="https://github.com/YsarocK/mansorygrid-maker" target="_blank">Code <GithubIcon class="w-[1rem]" /></a></p>
     </div>
   </footer>
 </template>
